eb95aa15c4d9be9da031e2ef8ae0eb42b98fd0f2
Kurian Vithayathil <k.vithayathil@gmail.com>
2017-09-26 12:44:53 -0400

    - Adding test for getting a token from shared prefs

--- app/src/test/java/com/koalatea/thehollidayinn/softwareengineeringdaily/data/preference/AuthPreferenceImplTest.java
+++ app/src/test/java/com/koalatea/thehollidayinn/softwareengineeringdaily/data/preference/AuthPreferenceImplTest.java
@@ -2,8 +2,24 @@ package com.koalatea.thehollidayinn.softwareengineeringdaily.data.preference;
 
 import com.koalatea.thehollidayinn.softwareengineeringdaily.test.BaseUnitTest;
 
+import org.junit.Test;
+import org.mockito.Spy;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
 /**
  * Created by Kurian on 25-Sep-17.
  */
 public class AuthPreferenceImplTest extends BaseUnitTest {
+
+    @Spy AuthPreferenceImpl prefs;
+
+    @Test
+    public void verify_getToken_gets_expected_value_from_preference() throws Exception {
+        doReturn("test").when(appComponent().sharedPreferences()).getString(anyString(),
+                anyString());
+        assertEquals("test", prefs.getToken());
+    }
 }
