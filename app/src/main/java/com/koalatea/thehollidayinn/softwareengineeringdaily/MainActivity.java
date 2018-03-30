05c6b76ccd40bb177078e3c0b197899418c68ee5
Keith Holliday <keithrholliday@gmail.com>
2018-03-30 16:01:32 -0500

    More style updates

--- app/src/main/java/com/koalatea/thehollidayinn/softwareengineeringdaily/MainActivity.java
+++ app/src/main/java/com/koalatea/thehollidayinn/softwareengineeringdaily/MainActivity.java
@@ -3,12 +3,15 @@ package com.koalatea.thehollidayinn.softwareengineeringdaily;
 import android.app.SearchManager;
 import android.arch.persistence.room.Room;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -149,9 +152,16 @@ public class MainActivity extends PlaybackControllerActivity
             loginItem.getName().setText(getString(R.string.logout));
             accountHeaderBuilder.addProfiles(
                 new ProfileDrawerItem()
-                        .withName("Logged In")
+                    .withName("Logged In")
 //                        .withEmail("mikepenz@gmail.com")
-                        .withIcon(getResources()
+                    .withIcon(getResources()
+                    .getDrawable(R.drawable.sedaily_logo))
+            );
+        } else {
+            accountHeaderBuilder.addProfiles(
+                new ProfileDrawerItem()
+                    .withName("LoggedOut")
+                    .withIcon(getResources()
                         .getDrawable(R.drawable.sedaily_logo))
             );
         }
@@ -188,6 +198,27 @@ public class MainActivity extends PlaybackControllerActivity
                     }
                 })
                 .build();
+    }
+
+    private void displayMessage (String message) {
+        AlertDialog.Builder builder;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        builder.setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     private Boolean navigationItemSelected(@NonNull IDrawerItem item) {
@@ -236,6 +267,11 @@ public class MainActivity extends PlaybackControllerActivity
                 startActivity(new Intent(this, SubscriptionActivity.class));
                 break;
             case 6:
+                if (userRepository.getToken().isEmpty()) {
+                    displayMessage("You must login to use this feature");
+                    break;
+                }
+
                 toolbar.setTitle("Bookmarks");
                 tabLayout.setVisibility(View.GONE);
 
