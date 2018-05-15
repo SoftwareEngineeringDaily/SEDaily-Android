410b57097c084660093262e1ddb304aca8ee13aa
Keith Holliday <keithrholliday@gmail.com>
2018-05-14 20:55:55 -0500

    Crash fixes

--- app/src/main/java/com/koalatea/thehollidayinn/softwareengineeringdaily/audio/QueueManager.java
+++ app/src/main/java/com/koalatea/thehollidayinn/softwareengineeringdaily/audio/QueueManager.java
@@ -77,10 +77,10 @@ public class QueueManager {
   public boolean skipQueuePosition(int amount) {
     int index = mCurrentIndex + amount;
 
-    if (index < 0) {
+    if (index <= 0) {
       // skip backwards before the first song will keep you on the first song
       index = 0;
-    } else {
+    } else if (mPlayingQueue.size() > 1) {
       // skip forwards when in last song will cycle back to start of the queue
       index %= mPlayingQueue.size();
     }
